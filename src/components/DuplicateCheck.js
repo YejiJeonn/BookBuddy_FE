17e63bec7e16b9cd732fc7c5e7111cef245249d4
yejiJeonn <yejijeon02@gmail.com>
2024-10-28 03:42:57 +0900

    비밀번호 일치 확인 완료 / 아이디 중복 확인 로직 개발 중

--- src/components/DuplicateCheck.js
+++ src/components/DuplicateCheck.js
@@ -1,15 +1,20 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
+// CheckDuplicateButton.js
+import React from 'react';
 
-function DuplicateCheck(props) {
-    const [isClicked, setIsClicked] = React.useState(false);
+const CheckDuplicateButton = ({ userId, onCheckDuplicate }) => {
+    const handleClick = () => {
+        if (userId) {
+            onCheckDuplicate(userId); // 부모 컴포넌트로부터 전달받은 함수 실행
+        } else {
+            alert("아이디를 입력하세요.");
+        }
+    };
 
-    return React.createElement(
-        'button',
-        {onClick: () => setIsClicked(true)},
-        isClicked ? '사용 가능' : '중복 확인'
-    )
-}
+    return (
+        <button onClick={handleClick}>
+            중복확인
+        </button>
+    );
+};
 
-const domContainer = document.querySelector('#root');
-ReactDOM.render(React.createElement(DuplicateCheck), domContainer);
+export default CheckDuplicateButton;
